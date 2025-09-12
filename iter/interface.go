783f7d2d45f6a5ea29193cb78ce5e0d7084d5a67
Brandon Chisham <bchisham@gmail.com>
2025-09-12 13:22:36 -0700

    Documents MapToSeq

--- iter/interface.go
+++ iter/interface.go
@@ -4,6 +4,7 @@ import (
 	"iter"
 )
 
+// MapToSeq applies a transformation function to each element of the input slice and returns a sequence of the results.
 func MapToSeq[U any, ISlice ~[]T, T any](s ISlice) func(xf func(T) U) iter.Seq[U] {
 	return func(xf func(T) U) iter.Seq[U] {
 		return func(yield func(U) bool) {
